7c05edcca4fadde211a555ae0c0bd9607db8d564
LeeJoobang <hii5074@gmail.com>
2023-04-06 14:38:03 +0900

    [add] storyboard + TableViewController, UIviewController 연결

---
+++ GoodWeather/GoodWeather/ViewController/AddCityViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddCityViewController.swift
+//  GoodWeather
+//
+//  Created by Joobang on 2023/04/06.
+//
+
+import Foundation
+import UIKit
+
+class AddCityViewController: UIViewController{
+    
+}
---
+++ GoodWeather/GoodWeather/ViewController/WeatherListTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeather
+//
+//  Created by Joobang on 2023/04/06.
+//
+
+import Foundation
+import UIKit
+
+class WeatherListTableViewController: UITableViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
